f54f499b6233ba26f3ecae2afea5e82b67300ecb
pauflorenti <pauf.97@gmail.com>
2023-10-03 23:06:33 +0200

    Adding player controller and player state

---
+++ project/Source/MoonAttacks/Player/MoonPlayerController.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "SogasGasSDK/Actors/SGSPlayerController.h"
+
+#include "MoonPlayerController.generated.h"
+
+UCLASS()
+class MOONATTACKS_API AMoonPlayerController : public ASGSPlayerController
+{
+    GENERATED_BODY()
+};
---
+++ project/Source/MoonAttacks/Player/MoonPlayerState.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "SogasGasSDK/Actors/SGSPlayerState.h"
+
+#include "MoonPlayerState.generated.h"
+
+UCLASS()
+class MOONATTACKS_API AMoonPlayerState : public ASGSPlayerState
+{
+	GENERATED_BODY()
+
+public:
+};
